fb695dbad3e94a72654b7d7ed125ec8b88385f3f
NLxs <15916584270@163.com>
2021-01-17 01:03:29 +0800

    引入css

--- src/x.js
+++ src/x.js
@@ -1,1 +1,5 @@
-export default 'xxx'
+// import css from './assets/x.css';
+// console.log(css); // 打印出数组
+// console.log(css.toString()); // 字符串
+import './assets/x.css';
+export default 'xxx';
--- webpack.config.js
+++ webpack.config.js
@@ -1,4 +1,5 @@
 const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
 
 module.exports = {
   mode: 'development',
@@ -6,5 +7,20 @@ module.exports = {
   output: {
     path: path.resolve(__dirname, 'dist'),
     filename: 'index.[contenthash].js',
-  }
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      title: '无限进步', // 模板页面标题
+      template: './src/assets/index.html' // 模板位置
+    })
+  ],
+  module: {
+    rules: [
+      {
+        test: /\.css$/i,
+        use: ["css-loader"],
+        //use: ["style-loader", "css-loader"],
+      },
+    ],
+  },
 };
